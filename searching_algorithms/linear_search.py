f33175a0d8092fb8c8a8b624fcb4c43ba788a9ea
anil-gaikwad <anilgaikwad2298@gmail.com>
2025-09-02 10:09:09 +0530

    add linked list

--- searching_algorithms/linear_search.py
+++ searching_algorithms/linear_search.py
@@ -1,16 +1,12 @@
-
-
 def linear_search(lst, target):
-    
-    n = len(lst)
-
-    for i in range(n):
-        if lst[i] == target:
+    """Return index of target in list, or -1 if not found. O(n) time."""
+    for i, value in enumerate(lst):
+        if value == target:
             return i
-    
     return -1
 
 
+# Example
 myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 myTarget = 15
-print(linear_search(myArray, myTarget))
+print(linear_search(myArray, myTarget))  # 7
