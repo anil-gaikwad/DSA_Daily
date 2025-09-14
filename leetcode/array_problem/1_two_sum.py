58286e5b976e7e6e6aa62389bbe837ed55aa4098
anil-gaikwad <anilgaikwad2298@gmail.com>
2025-09-14 21:32:19 +0530

    two sum

--- leetcode/array_problem/1_two_sum.py
+++ leetcode/array_problem/1_two_sum.py
@@ -2,6 +2,7 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+# 2. Hash Map Approach
 
 def two_sum(nums, target):
     maps = {}
@@ -21,3 +22,21 @@ print(two_sum(nums, target))
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)
+
+#-----------------------------------------------#
+# 2. Brute Force Approach
+def two_sum_brute_force(nums, target):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+nums = [2,7,11,15]
+target = 9
+print(two_sum_brute_force(nums, target))
+
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
