64c62f37d5be8e309a172339dd74042f546acba9
anil-gaikwad <anilgaikwad2298@gmail.com>
2025-12-02 10:12:27 +0530

    added problem

--- Interview_practice/heaps.py
+++ Interview_practice/heaps.py
@@ -1,54 +1,65 @@
+########################## 1. Kth Largest Element in an Array ########################## 
 # Q 1. Kth Largest Element in an Array
-# Given an integer array nums and an integer k, return the kth largest element
-# in the array.
-# Note that it is the kth largest element in the sorted order, not the kth distinct
-# element.
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
 # You must solve it in O(n) time complexity.
+
 # Example 1:
 # Input: nums = [3,2,1,5,6,4], k = 2
 # Output: 5
+#--------------------------- Solution --------------------------------# 
 
+
+##########################  2. Ugly Number II ########################## 
 
 # Q 2. Ugly Number II
-# An ugly number is a positive integer whose prime factors are limited to 2, 3,
-# and 5.
+# An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
 # Given an integer n, return the nth ugly number.
+
 # Example 1:
 # Input: n = 10
 # Output: 12
+#--------------------------- Solution --------------------------------# 
 
+
+########################## 3. Sort Characters By Frequency ########################## 
 
 # Q 3. Sort Characters By Frequency
 # Given a string s, sort it in decreasing order based on the frequency of the
-# characters. The frequency of a character is the number of times it appears in
-# the string.
+# characters. The frequency of a character is the number of times it appears in the string.
 # Return the sorted string. If there are multiple answers, return any of them.
+
 # Example 1:
 # Input: s = "tree"
 # Output: "eert"
+#--------------------------- Solution --------------------------------# 
+
+
+########################## 4. Top K Frequent Words ########################## 
 
 # Q 4. Top K Frequent Words
-# Given an array of strings words and an integer k, return the k most frequent
-# strings.
-# Return the answer sorted by the frequency from highest to lowest. Sort the
-# words with the same frequency by their lexicographical order.
+# Given an array of strings words and an integer k, return the k most frequent strings.
+# Return the answer sorted by the frequency from highest to lowest. Sort the words with 
+# the same frequency by their lexicographical order.
+
 # Example 1:
 # Input: words = ["i","love","leetcode","i","love","coding"], k = 2
 # Output: ["i","love"]
 
+#--------------------------- Solution --------------------------------# 
+
+########################## 5. Find Median from Data Stream ########################## 
 
 # Q 5. Find Median from Data Stream
 # The median is the middle value in an ordered integer list. If the size of the list
-# is even, there is no middle value, and the median is the mean of the two middle
-# values.
+# is even, there is no middle value, and the median is the mean of the two middle values.
 # • For example, for arr = [2,3,4], the median is 3.
 # • For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
 # Implement the MedianFinder class:
 # • MedianFinder() initializes the MedianFinder object.
-# • void addNum(int num) adds the integer num from the data stream to
-# the data structure.
-# • double findMedian() returns the median of all elements so far. Answers
-# within 10-5 of the actual answer will be accepted.
+# • void addNum(int num) adds the integer num from the data stream to the data structure.
+# • double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
 # Example 1:
 # Input
 # ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
@@ -56,55 +67,66 @@
 # Output
 # [null, null, null, 1.5, null, 2.0]
 
+#--------------------------- Solution --------------------------------# 
+
+########################## 6. Find K Pairs with Smallest Sums ########################## 
+
 # Q 6. Find K Pairs with Smallest Sums
-# You are given two integer arrays nums1 and nums2 sorted in ascending order
-# and an integer k.
-# Define a pair (u, v) which consists of one element from the first array and one
-# element from the second array.
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
 # Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
 # Example 1:
 # Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
 # Output: [[1,2],[1,4],[1,6]]
+#--------------------------- Solution --------------------------------# 
 
+########################## 7. Kth Smallest Element in a Sorted Matrix ########################## 
 
 # Q 7. Kth Smallest Element in a Sorted Matrix
-# Given an n x n matrix where each of the rows and columns is sorted in
-# ascending order, return the kth smallest element in the matrix.
-# Note that it is the kth smallest element in the sorted order, not the kth distinct
-# element.
+# Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+# Note that it is the kth smallest element in the sorted order, not the kth distinct element.
 # You must find a solution with a memory complexity better than O(n2).
 # Example 1:
 # Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
 # Output: 13
 
+#--------------------------- Solution --------------------------------# 
+
+########################## 8. Find K Pairs with Smallest Sums ########################## 
 
 # Q 8. Find K Pairs with Smallest Sums
-# You are given two integer arrays nums1 and nums2 sorted in ascending order
-# and an integer k.
-# Define a pair (u, v) which consists of one element from the first array and one
-# element from the second array.
+# You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+# Define a pair (u, v) which consists of one element from the first array and one element from the second array.
 # Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
 # Example 1:
 # Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
 # Output: [[1,2],[1,4],[1,6]]
 
+#--------------------------- Solution --------------------------------# 
+
+
+########################## 9. Kth Smallest Element in a Sorted Matrix ########################## 
 
 # Q 9. Kth Smallest Element in a Sorted Matrix
-# Given an n x n matrix where each of the rows and columns is sorted in
-# ascending order, return the kth smallest element in the matrix.
-# Note that it is the kth smallest element in the sorted order, not the kth distinct
-# element.
+# Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+# Note that it is the kth smallest element in the sorted order, not the kth distinct element.
 # You must find a solution with a memory complexity better than O(n2).
 # Example 1:
 # Input: matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
 # Output: 13
 
+#--------------------------- Solution --------------------------------# 
+
+
+########################## 10. Reorganize String ########################## 
 
 # Q 10. Reorganize String
-# Given a string s, rearrange the characters of s so that any two adjacent
-# characters are not the same.
+# Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
 # Return any possible rearrangement of s or return "" if not possible.
+
 # Example 1:
 # Input: s = "aab"
 # Output: "aba"
 
+#--------------------------- Solution --------------------------------# 
